31640b984fc31b063bf20128b67121a00ada7604
vitaliasvirskaja <vitalia.svirskaja.vs@gmail.com>
2023-01-28 00:20:23 +0100

    added font family nunito

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Nunito } from "@next/font/google";
+
+const nunito = Nunito({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={nunito.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
