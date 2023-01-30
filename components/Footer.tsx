4681e568fc4b0c905a8712d948ddad0ee59307cd
vitaliasvirskaja <vitalia.svirskaja.vs@gmail.com>
2023-01-31 00:15:09 +0100

    implemented first footer design

---
+++ components/Footer.tsx
@@ -0,0 +1,18 @@
+import { NavbarContent } from "./Navbar";
+import Link from "next/link";
+
+export const Footer = () => {
+  return (
+    <div className="bg-dark-grey p-6">
+      <div className="flex flex-col gap-8">
+        <div>
+          <NavbarContent />
+        </div>
+        <div className="text-white w-full max-w-2xl flex justify-between items-center m-auto">
+          <Link href="">Impressum</Link>
+          <Link href="">Datenschutz</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
