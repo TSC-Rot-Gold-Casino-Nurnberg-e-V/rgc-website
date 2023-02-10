4c2981b471ca8c0a54ff560e5ff8d1b77baaeb3a
vitaliasvirskaja <vitalia.svirskaja.vs@gmail.com>
2023-02-10 22:52:33 +0100

    implemented first layout of the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,10 +4,7 @@ export const Footer = () => {
   return (
     <div className="bg-dark-grey p-6">
       <div className="flex flex-col gap-8">
-        <div>
-          <NavbarContent />
-        </div>
-        <div className="text-white w-full max-w-2xl flex justify-between items-center m-auto">
+        <div className="text-white w-full max-w-screen-lg flex justify-between items-center m-auto">
           <Link href="">Impressum</Link>
           <Link href="">Datenschutz</Link>
           <Link href="">Kontakt</Link>
