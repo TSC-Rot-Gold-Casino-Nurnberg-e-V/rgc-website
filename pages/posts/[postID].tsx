2c4760b713f7be6e149b42a3f1f5baad8e3a9f1b
vitaliasvirskaja <vitalia.svirskaja.vs@gmail.com>
2023-02-15 20:59:34 +0100

    exchanged react markdown through sanitized html from ckeditor

--- pages/posts/[postID].tsx
+++ pages/posts/[postID].tsx
@@ -1,10 +1,9 @@
-import ReactMarkdown from "react-markdown";
 import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from "next";
 import { Post } from "../../model/Post";
 import { getPost, getPosts } from "../../api/api";
-import remarkGfm from "remark-gfm";
 import { Navbar } from "../../components/Navbar";
 import { Footer } from "../../components/Footer";
+import { sanitizeHTMLField } from "../../utils/sanitizeHTMLField";
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const posts = await getPosts();
@@ -30,19 +29,19 @@ export default function PostID({
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <div className="py-8 max-w-3xl m-auto bg-white grow">
-        <h1 className="text-red-900 text-center text-3xl">
+      <main className="py-8 max-w-3xl m-auto grow">
+        <h1 className="text-red-900 text-center text-3xl py-4">
           {post.attributes.title}
         </h1>
         <div className="prose m-auto">
-          <ReactMarkdown
-            className="prose-img:m-auto prose-p:text-justify prose-img:rounded-lg prose-img:max-w-3xl prose-img:max-h-96"
-            remarkPlugins={[remarkGfm]}
-          >
-            {post.attributes.description}
-          </ReactMarkdown>
+          <div
+            className="prose prose-img:m-auto prose-img:max-w-3xl prose-p:text-justify prose-img:rounded-lg prose-img:max-h-96"
+            dangerouslySetInnerHTML={{
+              __html: sanitizeHTMLField(post.attributes.description),
+            }}
+          />
         </div>
-      </div>
+      </main>
       <Footer />
     </div>
   );
