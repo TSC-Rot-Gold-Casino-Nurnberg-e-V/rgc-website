b14cfa7f85a4b28c447f40a96ccf061f422c80e9
vitaliasvirskaja <vitalia.svirskaja.vs@gmail.com>
2023-02-19 18:19:27 +0100

    refactoring

--- model/History.ts
+++ model/History.ts
@@ -1,10 +1,10 @@
 import { z } from "zod";
 
-export type History = z.infer<typeof historySchema>;
-
 export const historySchema = z.object({
   id: z.number(),
   attributes: z.object({
     content: z.string(),
   }),
 });
+
+export type History = z.infer<typeof historySchema>;
