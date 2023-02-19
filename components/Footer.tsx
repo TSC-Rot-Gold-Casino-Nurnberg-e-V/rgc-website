9e5b2bad0a0bf981636b24361f8e16b6a4f988c7
vitaliasvirskaja <vitalia.svirskaja.vs@gmail.com>
2023-02-19 18:05:53 +0100

    refactoring

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,15 +1,13 @@
 import Link from "next/link";
 
-export const Footer = () => {
-  return (
-    <div className="bg-zinc-700 p-6">
-      <div className="flex flex-col gap-8">
-        <div className="text-white w-full max-w-screen-lg flex justify-between items-center m-auto">
-          <Link href="">Impressum</Link>
-          <Link href="">Datenschutz</Link>
-          <Link href="">Kontakt</Link>
-        </div>
+export const Footer = () => (
+  <div className="bg-zinc-700 p-6">
+    <div className="flex flex-col gap-8">
+      <div className="text-white w-full max-w-screen-lg flex justify-between items-center m-auto">
+        <Link href="">Impressum</Link>
+        <Link href="">Datenschutz</Link>
+        <Link href="">Kontakt</Link>
       </div>
     </div>
-  );
-};
+  </div>
+);
