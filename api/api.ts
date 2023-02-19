139777fdb1916c05b89c93fc563d1973dc2f6493
vitaliasvirskaja <vitalia.svirskaja.vs@gmail.com>
2023-02-19 18:03:58 +0100

    refactoring

--- api/api.ts
+++ api/api.ts
@@ -10,14 +10,14 @@ export async function getPosts(): Promise<Array<Post>> {
   const urlSearchParams = new URLSearchParams();
   urlSearchParams.append("sort", "chronologicalPosition:desc");
   urlSearchParams.append("populate", "*");
-  const data = fetchData(`/posts?${urlSearchParams}`);
+  const data = await fetchData(`/posts?${urlSearchParams}`);
   return postsSchema.parse(data);
 }
 
 export async function getPost(postID: string): Promise<Post> {
   const urlSearchParams = new URLSearchParams();
   urlSearchParams.append("populate", "*");
-  const data = fetchData(`/posts/${postID}?${urlSearchParams}`);
+  const data = await fetchData(`/posts/${postID}?${urlSearchParams}`);
   return postSchema.parse(data);
 }
 
@@ -28,33 +28,29 @@ export async function getEvents(): Promise<Array<Event>> {
   const isoDate = new Date().toISOString().substring(0, 10);
   const property: keyof Event["attributes"] = "eventStartDate";
   urlSearchParams.append(`filters[${property}][$gte]`, isoDate);
-  const data = fetchData(`/events?${urlSearchParams}`);
+  const data = await fetchData(`/events?${urlSearchParams}`);
   return eventsSchema.parse(data);
 }
 
 export async function getEvent(eventID: string): Promise<Event> {
   const urlSearchParams = new URLSearchParams();
   urlSearchParams.append("populate", "*");
-  const data = fetchData(`/events/${eventID}?${urlSearchParams}`);
+  const data = await fetchData(`/events/${eventID}?${urlSearchParams}`);
   return eventSchema.parse(data);
 }
 
 export async function getHistory(): Promise<History> {
-  const data = fetchData("/history");
+  const data = await fetchData("/history");
   return historySchema.parse(data);
-}
-
-async function handleError(res: Response) {
-  if (!res.ok) {
-    const error = await res.json();
-    throw new Error(JSON.stringify(error));
-  }
 }
 
 async function fetchData(path: string) {
   const res = await fetch(baseUrl + path, {
     headers: headers,
   });
-  await handleError(res);
+  if (!res.ok) {
+    const error = await res.json();
+    throw new Error(JSON.stringify(error));
+  }
   return (await res.json()).data;
 }
