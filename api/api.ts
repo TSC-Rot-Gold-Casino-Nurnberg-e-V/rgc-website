d825fc185335939fd9ebb9ec13bdf1aeb94314d7
vitaliasvirskaja <vitalia.svirskaja.vs@gmail.com>
2023-02-19 18:02:00 +0100

    refactoring

--- api/api.ts
+++ api/api.ts
@@ -6,34 +6,19 @@ const baseUrl = `${process.env.CMS_URL}/api`;
 const headers = new Headers();
 headers.append("Authorization", `Bearer ${process.env.CMS_TOKEN}`);
 
-async function handleError(res: Response) {
-  if (!res.ok) {
-    const error = await res.json();
-    throw new Error(JSON.stringify(error));
-  }
-}
-
 export async function getPosts(): Promise<Array<Post>> {
   const urlSearchParams = new URLSearchParams();
   urlSearchParams.append("sort", "chronologicalPosition:desc");
   urlSearchParams.append("populate", "*");
-  const res = await fetch(`${baseUrl}/posts?${urlSearchParams}`, {
-    headers: headers,
-  });
-  await handleError(res);
-  const json = await res.json();
-  return postsSchema.parse(json.data);
+  const data = fetchData(`/posts?${urlSearchParams}`);
+  return postsSchema.parse(data);
 }
 
 export async function getPost(postID: string): Promise<Post> {
   const urlSearchParams = new URLSearchParams();
   urlSearchParams.append("populate", "*");
-  const res = await fetch(`${baseUrl}/posts/${postID}?${urlSearchParams}`, {
-    headers: headers,
-  });
-  await handleError(res);
-  const json = await res.json();
-  return postSchema.parse(json.data);
+  const data = fetchData(`/posts/${postID}?${urlSearchParams}`);
+  return postSchema.parse(data);
 }
 
 export async function getEvents(): Promise<Array<Event>> {
@@ -43,30 +28,33 @@ export async function getEvents(): Promise<Array<Event>> {
   const isoDate = new Date().toISOString().substring(0, 10);
   const property: keyof Event["attributes"] = "eventStartDate";
   urlSearchParams.append(`filters[${property}][$gte]`, isoDate);
-  const res = await fetch(`${baseUrl}/events?${urlSearchParams}`, {
-    headers: headers,
-  });
-  await handleError(res);
-  const json = await res.json();
-  return eventsSchema.parse(json.data);
+  const data = fetchData(`/events?${urlSearchParams}`);
+  return eventsSchema.parse(data);
 }
 
 export async function getEvent(eventID: string): Promise<Event> {
   const urlSearchParams = new URLSearchParams();
   urlSearchParams.append("populate", "*");
-  const res = await fetch(`${baseUrl}/events/${eventID}?${urlSearchParams}`, {
-    headers: headers,
-  });
-  await handleError(res);
-  const json = await res.json();
-  return eventSchema.parse(json.data);
+  const data = fetchData(`/events/${eventID}?${urlSearchParams}`);
+  return eventSchema.parse(data);
 }
 
 export async function getHistory(): Promise<History> {
-  const res = await fetch(`${baseUrl}/history`, {
+  const data = fetchData("/history");
+  return historySchema.parse(data);
+}
+
+async function handleError(res: Response) {
+  if (!res.ok) {
+    const error = await res.json();
+    throw new Error(JSON.stringify(error));
+  }
+}
+
+async function fetchData(path: string) {
+  const res = await fetch(baseUrl + path, {
     headers: headers,
   });
   await handleError(res);
-  const json = await res.json();
-  return historySchema.parse(json.data);
+  return (await res.json()).data;
 }
