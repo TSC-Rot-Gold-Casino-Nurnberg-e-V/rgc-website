b996f08e9b5f7e4ed06babe7b7321549c3d89cd2
vitaliasvirskaja <vitalia.svirskaja.vs@gmail.com>
2023-03-07 12:05:33 +0100

    implemented generic page for competition results and planned competitions

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,41 +1,63 @@
 import { GetStaticProps, InferGetStaticPropsType } from "next";
-import { getEvents } from "../../api/api";
-import { Event } from "../../model/Event";
 import { Navbar } from "../../components/Navbar";
 import { Footer } from "../../components/Footer";
-import { EventCard } from "../../components/EventCard";
+import Link from "next/link";
+import Image from "next/image";
+import trophyImage from "../../public/trophy.jpg";
+import eventImage from "../../public/eventImage.png";
 
-export const getStaticProps: GetStaticProps<{ events: Event[] }> = async () => {
-  const events = await getEvents();
-  const dayInSeconds = 60 * 60 * 24;
+export const getStaticProps: GetStaticProps = async () => {
   return {
-    props: { events: events, revalidate: dayInSeconds },
+    props: {},
   };
 };
 
-export default function Events({
-  events,
-}: InferGetStaticPropsType<typeof getStaticProps>) {
+export default function Events({}: InferGetStaticPropsType<
+  typeof getStaticProps
+>) {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <div className="bg-stone-50 grow">
-        <main className="mx-auto p-6 md:p-8">
-          <div className="flex flex-col gap-8 max-w-3xl m-auto">
-            {events.map((event) => (
-              <EventCard
-                eventID={event.id}
-                title={event.attributes.title}
-                previewText={event.attributes.previewText}
-                startDate={new Date(event.attributes.startDate)}
-                endDate={
-                  event.attributes.endDate !== null
-                    ? new Date(event.attributes.endDate)
-                    : null
-                }
-                key={event.id}
-              />
-            ))}
+      <div className="bg-stone-50 grow flex items-center">
+        <main className="mx-auto max-w-3xl bg-stone-50 grow">
+          <div className="flex justify-between items-center w-full gap-8">
+            <Link
+              href="/events/competitionResults"
+              className="relative flex flex-col items-center justify-center group grow"
+            >
+              <div className="relative h-80 w-full">
+                <Image
+                  src={trophyImage}
+                  alt=""
+                  fill
+                  className="object-cover bg-center rounded rounded-md"
+                />
+              </div>
+              <div className="backdrop-brightness-50 group-hover:backdrop-brightness-75 duration-700 absolute h-full flex flex-col justify-center w-full rounded-md">
+                <h2 className="text-white text-2xl font-bold text-center py-5 px-2 w-full">
+                  Turnierergebnisse
+                </h2>
+              </div>
+            </Link>
+
+            <Link
+              href="/events/competitionResults"
+              className="relative flex flex-col items-center justify-center group grow"
+            >
+              <div className="relative h-80 w-full">
+                <Image
+                  src={eventImage}
+                  alt=""
+                  fill
+                  className="object-cover bg-center rounded rounded-md"
+                />
+              </div>
+              <div className="backdrop-brightness-50 group-hover:backdrop-brightness-75 duration-700 absolute h-full flex flex-col justify-center w-full rounded-md">
+                <h2 className="text-white text-2xl font-bold text-center py-5 px-2 w-full">
+                  Veranstaltungen
+                </h2>
+              </div>
+            </Link>
           </div>
         </main>
       </div>
