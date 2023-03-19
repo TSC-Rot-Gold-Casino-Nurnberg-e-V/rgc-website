1f0bda500b288f3449df36d3bd65095dd6ca98fe
vitaliasvirskaja <vitalia.svirskaja.vs@gmail.com>
2023-03-19 11:05:28 +0100

    added global meta tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,21 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Nunito } from "@next/font/google";
+import Head from "next/head";
 
 const nunito = Nunito({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={nunito.className}>
-      <Component {...pageProps} />
-    </div>
+    <>
+      <Head>
+        <title>TSC Rot-Gold-Casino Nürnberg e.V.</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={nunito.className}>
+        <Component {...pageProps} />
+      </div>
+    </>
   );
 }
