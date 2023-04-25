3b6e1800012ddbb9332ce54feed87db467f44909
vitaliasvirskaja <vitalia.svirskaja.vs@gmail.com>
2023-04-25 23:07:33 +0200

    refactoring

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,16 +1,14 @@
-import { HTMLAttributes, PropsWithChildren } from "react";
+import { ButtonHTMLAttributes, PropsWithChildren } from "react";
 
 export const Button = ({
   children,
   className,
   ...rest
-}: PropsWithChildren<HTMLAttributes<HTMLButtonElement>>) => {
-  return (
-    <button
-      className="w-fit rounded-md bg-accent px-6 py-4 font-bold text-dark-grey"
-      {...rest}
-    >
-      {children}
-    </button>
-  );
-};
+}: PropsWithChildren<ButtonHTMLAttributes<HTMLButtonElement>>) => (
+  <button
+    className="primary-gradient w-fit rounded-md px-6 py-4 font-bold text-base-800 text-rose-950 shadow transition-all hover:scale-105 hover:text-rose-900 hover:shadow-md hover:shadow-rose-900/10"
+    {...rest}
+  >
+    {children}
+  </button>
+);
