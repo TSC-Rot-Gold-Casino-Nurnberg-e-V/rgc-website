4b6ac887fd9bb88e65666aedcb4f57a4c4ccc77b
vitaliasvirskaja <vitalia.svirskaja.vs@gmail.com>
2023-04-25 23:02:16 +0200

    changed background color
    #26

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -19,10 +19,10 @@ export const NavLink = forwardRef<HTMLAnchorElement, NavLinkProps>(
     return (
       <li className={shouldHideOnSmallViewport ? "max-lg:hidden" : ""}>
         <Link
-          className={`whitespace-nowrap rounded-md px-6 py-2 transition-all hover:text-gray-50 md:px-3 lg:px-4 ${
+          className={`whitespace-nowrap rounded-md px-6 py-2 transition-all hover:text-base-50 md:px-3 lg:px-4 ${
             isActive
-              ? "underline decoration-gray-200 decoration-2 underline-offset-8 "
-              : "text-gray-300"
+              ? "underline decoration-base-200 decoration-2 underline-offset-8 "
+              : "text-base-300"
           } ${className}`}
           {...rest}
           href={href}
@@ -49,7 +49,7 @@ const MenuLink = ({ text, href }: MenuLinkProps) => (
       <NavLink
         text={text}
         href={href}
-        className={`rounded-2xl ${active ? "bg-gray-800" : ""}`}
+        className={`rounded-2xl ${active ? "bg-base-800" : ""}`}
       />
     )}
   </Menu.Item>
@@ -59,7 +59,7 @@ export const Navbar = () => {
   const hideNavbar = useHideNavbar();
   return (
     <nav
-      className={`sticky h-20 w-full bg-primary px-8 text-gray-50 ${
+      className={`sticky h-20 w-full bg-base-950 px-8 text-base-50 ${
         hideNavbar ? "-top-20" : "top-0"
       } z-30 transition-all duration-500`}
     >
@@ -87,7 +87,7 @@ export const Navbar = () => {
             {({ open }) => (
               <>
                 <Menu.Button
-                  className="rounded-md p-2 hover:bg-gray-700 focus:bg-gray-800"
+                  className="rounded-md p-2 hover:bg-base-700 focus:bg-base-800"
                   aria-label={`Navigationsmenü ${
                     open ? "schließen" : "öffnen"
                   }`}
@@ -117,7 +117,7 @@ export const Navbar = () => {
                   leaveFrom="transform scale-100 opacity-100"
                   leaveTo="transform scale-95 opacity-0"
                 >
-                  <Menu.Items className="text-md menu rounded-box bg-gray-700 py-2 text-gray-50">
+                  <Menu.Items className="text-md menu rounded-box bg-base-700 py-2 text-base-50">
                     <MenuLink text="Der Verein" href="/association" />
                     <MenuLink text="News" href="/posts" />
                     <MenuLink text="Angebot" href="/courses" />
