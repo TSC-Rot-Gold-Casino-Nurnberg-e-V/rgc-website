013d049ecb5b963cd86ce68dc300ef5e6d55cadb
vitaliasvirskaja <vitalia.svirskaja.vs@gmail.com>
2023-04-25 23:04:28 +0200

    refactoring

--- tailwind.config.js
+++ tailwind.config.js
@@ -40,24 +40,5 @@ module.exports = {
       },
     },
   },
-  daisyui: {
-    themes: [
-      {
-        goldTheme: {
-          primary: "#04060a",
-          secondary: "#9A917B",
-          accent: "#E0C280",
-          lightgrey: "#B1B8C6",
-          backgroundImage: {
-            hero: "../public/heroBanner.png",
-          },
-        },
-      },
-    ],
-  },
-  plugins: [
-    require("@tailwindcss/typography"),
-    require("@tailwindcss/line-clamp"),
-    require("daisyui"),
-  ],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
 };
