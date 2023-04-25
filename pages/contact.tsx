d1186416af29f6ea069343e4f0ec236eadb6ffc5
vitaliasvirskaja <vitalia.svirskaja.vs@gmail.com>
2023-04-25 23:24:37 +0200

    refactored

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,9 +1,9 @@
-import { Contactform } from "../components/Contactform";
+import { ContactForm } from "../components/ContactForm";
 
 export default function Contact() {
   return (
     <main className="mx-auto max-w-3xl grow bg-stone-50 p-6 md:p-8">
-      <Contactform />
+      <ContactForm />
     </main>
   );
 }
