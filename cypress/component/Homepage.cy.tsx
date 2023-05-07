028258f05b15d2dd553b69327bb9f5a4a54d88a7
vitaliasvirskaja <vitalia.svirskaja.vs@gmail.com>
2023-05-07 18:25:57 +0200

    implemented component testing for homepage

---
+++ cypress/component/Homepage.cy.tsx
@@ -0,0 +1,230 @@
+import Home from "../../pages";
+
+const posts = [
+  {
+    id: 12,
+    attributes: {
+      title: "Marius und Kristina erneut Weltmeister Latein",
+      description:
+        '<p><img src="https://res.cloudinary.com/dlak0pmbw/image/upload/v1674838905/2040481_2bb81ddc94.jpg" alt="2040481.jpeg"></p><p>&nbsp;</p><p>Im Rahmen des WinterDanceFestivals in Mülheim an der Ruhr fand gestern die Weltmeisterschaft in den latein-amerikanischen Tänzen statt. Unter den 65 Paaren aus 41 Ländern die Deutschen Meister Marius-Andrei Balan und Kristina Moshenska. Mit fast fünf Punkten Vorsprung gewannen die Neu-Bayern die Goldmedaille und konnte damit ihren Titel vom letzten Jahr unangefochten verteidigen. Wir gratulieren ganz herzlich und freuen uns sehr, dieses tolle Paar in unserem Verein willkommen zu heißen!</p>',
+      publishedAt: "2023-01-23T17:20:36.827Z",
+      previewText:
+        "Somit konnte das Paar den Titel vom letzten Jahr unangefochten verteidigen.",
+      mainImage: {
+        data: {
+          attributes: {
+            formats: {
+              small: {
+                url: "https://res.cloudinary.com/dlak0pmbw/image/upload/v1674839002/small_2040481_0a1cb6682d.jpg",
+              },
+            },
+            url: "https://res.cloudinary.com/dlak0pmbw/image/upload/v1674839002/2040481_0a1cb6682d.jpg",
+          },
+        },
+      },
+    },
+  },
+  {
+    id: 13,
+    attributes: {
+      title: "Wir gewinnen den Bayernpokal der U19",
+      description:
+        '<p>Nach dem letzten Turnier der Bayernpokalserie der U19 in München gewannen wir mit Abstand und konnten endlich unseren diesjährigen Vereinspokal mitnehmen.&nbsp;</p><p>Die Gesamtendwertung ist hier gelistet (genauere Auflistung siehe unten):</p><p><img src="https://res.cloudinary.com/dlak0pmbw/image/upload/v1675097648/2022_11_13_Bayernpokal_Vereinswertung_1ffe1d7335.jpg" alt="2022-11-13-Bayernpokal-Vereinswertung.jpg"></p><ol><li><strong>TSC Rot-Gold-Casino Nürnberg</strong> (144 Punkte)</li><li>TSA Schwarz-Gold d. ESV Ingolstadt (129 Punkte)</li><li>Gelb-Schwarz-Casino München (97 Punkte)</li></ol><p>Weitere Informationen:<a href="https://ltvb.de/wp-content/uploads/2022/11/Bayernpokal-u19-Endergebnis.-2022.pdf">https://ltvb.de/wp-content/uploads/2022/11/Bayernpokal-u19-Endergebnis.-2022.pdf</a></p>',
+      publishedAt: "2023-01-23T17:21:08.466Z",
+      previewText:
+        "Der Vereinspokal geht mit dem Sieg der Bayernpokalserie der U19 an den RGC.",
+      mainImage: {
+        data: {
+          attributes: {
+            formats: {
+              small: {
+                url: "https://res.cloudinary.com/dlak0pmbw/image/upload/v1674840135/small_2022_11_13_Bayernpokal_Vereinswertung_0bca8ae172.jpg",
+              },
+            },
+            url: "https://res.cloudinary.com/dlak0pmbw/image/upload/v1674840135/2022_11_13_Bayernpokal_Vereinswertung_0bca8ae172.jpg",
+          },
+        },
+      },
+    },
+  },
+  {
+    id: 14,
+    attributes: {
+      title: "Lea und Dominik auf dem WDSF World Open new series Standard",
+      description:
+        '<p><img src="https://res.cloudinary.com/dlak0pmbw/image/upload/v1675000522/2022_11_26_WDSF_Open_Std_Timisoara_Depner_Baier_aa8c32ea76.jpg" alt="2022-11-26-WDSF-Open-Std-Timisoara-Depner-Baier.jpeg"></p><p>Dominik Depner und Lea Baier erreichten den 21.-22. Platz auf dem WDSF World Open new series Standard in Timisoara (Rumänien). Weiter so!&nbsp;</p>',
+      publishedAt: "2023-01-29T13:52:15.033Z",
+      previewText:
+        "Dominik Depner und Lea Baier erreichten den 21.-22. Platz auf dem WDSF World Open new series Standard in Timisoara (Rumänien). ",
+      mainImage: {
+        data: {
+          attributes: {
+            formats: {},
+            url: "https://res.cloudinary.com/dlak0pmbw/image/upload/v1675000437/2022_11_26_WDSF_Open_Std_Timisoara_Depner_Baier_f0d96b5bda.jpg",
+          },
+        },
+      },
+    },
+  },
+  {
+    id: 11,
+    attributes: {
+      title: "Unser Latein A-Team gewinnt den 2. Platz in Ludwigsburg",
+      description:
+        '<p>Das von Sean Welton und Angela Diez trainierte <strong>A-Team der Lateinformationen</strong> konnte auf seinem Auftaktturnier der 2. Bundesliga Süd Latein am 08.01. in der Rundsporthalle in Ludwigsburg sehr überzeugen. Das Team erreichte mit seiner wirklich mitreißenden neuen Choreographie „James Bond“ und einer insbesondere tänzerisch sehr überzeugenden Leistung den <strong>2. Platz</strong>, auch hier nur knapp (3:2) geschlagen vom Heimteam aus Ludwigsburg.</p><p><img src="https://res.cloudinary.com/dlak0pmbw/image/upload/v1674576434/A_Latein_d181c90433.jpg" alt="A-Latein.jpg"></p><p>Zudem tanzte auch unsere <strong>B-Formation</strong>, eine unserer Nachwuchsmannschaften, unter der Leitung von Michael Ortlieb und Hendrik Decker auf dem besagten Turnier und erlangte mit der Choreografie "Burlesque" den <strong>7. Platz</strong>. Wer unsere Formationen live erleben möchte, sollte am 4.3. und 5.3. sich zur BBZ Halle in Nürnberg begeben. Denn dort findet unser alljährliches Formationswochenende statt! Platzierungen des Turniers:&nbsp;</p><ol><li>TSC Residenz Ludwigsburg</li><li>TSC Rot-Gold-Casino Nürnberg A-Team</li><li>Formation c&nbsp;</li><li>Formation d&nbsp;</li><li>Formation e&nbsp;</li><li>Formation f&nbsp;</li><li>TSC Rot-Gold-Casino Nürnberg B-Team</li></ol>',
+      publishedAt: "2023-01-23T17:19:06.871Z",
+      previewText:
+        "Unser A-Team der Lateinformationen konnte auf seinem Auftaktturnier der 2. Bundesliga Süd Latein am 08.01. in Ludwigsburg mit seiner neuen Choreographie „James Bond“ den 2. Platz erreichen.",
+      mainImage: {
+        data: {
+          attributes: {
+            formats: {
+              small: {
+                url: "https://res.cloudinary.com/dlak0pmbw/image/upload/v1674576434/small_A_Latein_d181c90433.jpg",
+              },
+            },
+            url: "https://res.cloudinary.com/dlak0pmbw/image/upload/v1674576434/A_Latein_d181c90433.jpg",
+          },
+        },
+      },
+    },
+  },
+];
+
+describe("homepage", () => {
+  beforeEach(() => {
+    cy.mount(<Home posts={posts} />);
+  });
+
+  describe("herosection", () => {
+    it("should have a welcome heading", () => {
+      cy.findByRole("banner").findByRole("heading", {
+        name: /Herzlich Willkommen im RGC/i,
+      });
+    });
+
+    it("should contain a subheading", () => {
+      cy.findByRole("banner").findByRole("heading", {
+        name: /Ihrem Verein für Tanzsport in Nürnberg/i,
+      });
+    });
+
+    it("should contain a CTA linking to the courses page", () => {
+      cy.findByRole("banner")
+        .findByRole("button", { name: /Komm vorbei/i })
+        .findByRole("link")
+        .should("have.attr", "href", "/courses");
+    });
+
+    it("should contain a description", () => {
+      cy.findByRole("banner").contains(
+        "Erleben Sie die faszinierende Welt des Tanzens! Bei uns finden Sie alles, was das Tänzerherz begehrt - von Latein, Standard und Formationstanzen bis hin zu Breitensport, Kindertanzen und Breakdance. Werden Sie Teil unserer Tanzfamilie und erleben Sie mit uns unvergessliche Momente auf der Tanzfläche."
+      );
+    });
+  });
+
+  describe("Coursesection", () => {
+    it("should display the introduction to the courses", () => {
+      cy.findByRole("main")
+        .findByLabelText("Kursangebote")
+        .findByRole("heading", {
+          name: /Unser Angebot/i,
+        });
+      cy.findByRole("main")
+        .findByLabelText("Angebotsbeschreibung")
+        .should(
+          "have.text",
+          "Bei uns finden Sie alles, was das Tänzerherz begehrt - von Latein, Standard und Formationstanzen bis hin zu Breitensport, Kindertanzen und Breakdance."
+        );
+    });
+
+    it("should display the course Einzeltanz with the correct linking", () => {
+      cy.findByRole("main")
+        .findByLabelText("Kursangebote")
+        .findByRole("link", { name: /Einzeltanz/i })
+        .should("have.attr", "href", "/courses");
+    });
+
+    it("should display the course Kindertanzen with the correct linking", () => {
+      cy.findByRole("main")
+        .findByLabelText("Kursangebote")
+        .findByRole("link", { name: /Kindertanzen/i })
+        .should("have.attr", "href", "/courses");
+    });
+
+    it("should display the course Formation with the correct linking", () => {
+      cy.findByRole("main")
+        .findByLabelText("Kursangebote")
+        .findByRole("link", { name: /Formation/i })
+        .should("have.attr", "href", "/courses");
+    });
+  });
+
+  describe("Associationsection", () => {
+    it("should have two headings ", () => {
+      cy.findByRole("main")
+        .findByLabelText("Vereinsinformationen")
+        .findByRole("heading", { name: /Mit Herz und Rhythmus/i });
+      cy.findByRole("main")
+        .findByLabelText("Vereinsinformationen")
+        .findByRole("heading", { name: /Über uns/i });
+    });
+    it("should have a description", () => {
+      cy.findByRole("main")
+        .findByLabelText("Vereinsgeschichte")
+        .should(
+          "have.text",
+          "Wir sind der RGC Nürnberg - ein Verein, der die Leidenschaft für den Tanzsport in all seinen Facetten vereint. Seit unserer Gründung im Jahr 1963 steht die Freude am Tanzen und die Förderung der Tanzkultur im Mittelpunkt unserer Arbeit."
+        );
+    });
+    it("should have a CTA with linking to the association page", () => {
+      cy.findByRole("main")
+        .findByLabelText("Vereinsinformationen")
+        .findByRole("button")
+        .findByRole("link", { name: /Mehr erfahren/i })
+        .should("have.attr", "href", "/association");
+    });
+  });
+
+  describe("Statssection", () => {
+    it("should contain the stat regarding the member count", () => {
+      cy.findByRole("main")
+        .findByLabelText("Vereinsstatistik")
+        .should("contain", ">600")
+        .should("contain", "Mitglieder");
+    });
+
+    it("should contain the stat regarding the formation team count", () => {
+      cy.findByLabelText("Vereinsstatistik")
+        .should("contain", "8")
+        .should("contain", "Formationsteams");
+    });
+
+    it("should contain the stat regarding the trophy count", () => {
+      cy.findByLabelText("Vereinsstatistik")
+        .should("contain", "25x")
+        .should("contain", "Bayernpokalsieger");
+    });
+  });
+
+  describe("Newssection", () => {
+    it("should display the latest three news", () => {
+      cy.findByLabelText("Newssection").findByRole("heading", {
+        name: /News/i,
+      });
+      cy.findAllByLabelText("News").should("have.lengthOf", 3);
+      cy.findByLabelText("Newssection").should(
+        "contain",
+        "Marius und Kristina erneut Weltmeister Latein"
+      );
+      cy.findByLabelText("Newssection").should(
+        "contain",
+        "Wir gewinnen den Bayernpokal der U19"
+      );
+      cy.findByLabelText("Newssection").should(
+        "contain",
+        "Lea und Dominik auf dem WDSF World Open new series Standard"
+      );
+    });
+  });
+});
