b69fd76fa2b9e42898c23b6066a195c1df3ac7ef
vitaliasvirskaja <vitalia.svirskaja.vs@gmail.com>
2023-05-16 16:25:02 +0200

    changed background color (wip)

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -2,8 +2,10 @@ import { ContactForm } from "../components/ContactForm";
 
 export default function Contact() {
   return (
-    <main className="mx-auto max-w-3xl grow bg-stone-50 p-6 md:p-8">
-      <ContactForm />
-    </main>
+    <div className="bg-base-500">
+      <main className="mx-auto max-w-3xl grow bg-base-500 p-6 md:p-8">
+        <ContactForm />
+      </main>
+    </div>
   );
 }
