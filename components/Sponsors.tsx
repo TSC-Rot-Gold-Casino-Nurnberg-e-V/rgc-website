aaa6469de4660f00419f6ec7ceab2c5b5dd37a09
vitaliasvirskaja <vitalia.svirskaja.vs@gmail.com>
2023-05-21 11:21:29 +0200

    extracted component for sponsors

---
+++ components/Sponsors.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import Image from "next/image";
+import sbbLogo from "../public/sbbLogo.png";
+import aragLogo from "../public/aragLogo.jpg";
+import teamNbgLogo from "../public/teamnuernbergLogo.jpg";
+
+export const Sponsors = () => (
+  <section className="default-padding bg-base-950 py-12">
+    <div className="mx-auto flex max-w-screen-lg flex-wrap items-center justify-around gap-6 gap-y-12">
+      <Link href="https://www.sportbuendnis-bundesliga.de/" target="_blank">
+        <Image src={sbbLogo} alt="" width={208} />
+      </Link>
+      <Link href="https://www.arag-partner.de/gunnar-zidella/" target="_blank">
+        <Image src={aragLogo} alt="" width={208} />
+      </Link>
+      <Link
+        href="https://www.nuernberg.de/internet/team_nuernberg/"
+        target="_blank"
+      >
+        <Image src={teamNbgLogo} alt="" width={208} />
+      </Link>
+    </div>
+  </section>
+);
