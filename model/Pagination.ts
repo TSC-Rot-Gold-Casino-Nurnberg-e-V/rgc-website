65c1821c2b79d46e258b97371ac39b0b0e5422ff
vitaliasvirskaja <vitalia.svirskaja.vs@gmail.com>
2023-05-29 11:33:44 +0200

    #37 implemented pagination zod schema

---
+++ model/Pagination.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const paginationSchema = z.object({
+  page: z.number(),
+  pageSize: z.number(),
+  pageCount: z.number(),
+  total: z.number(),
+});
+
+export type Pagination = z.infer<typeof paginationSchema>;
