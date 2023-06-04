66dacf2f124e0f09568891273352c713b0d583dc
vitaliasvirskaja <vitalia.svirskaja.vs@gmail.com>
2023-06-04 15:44:47 +0200

    #37 inlined post card

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -74,3 +74,56 @@ export default function Posts({
     </>
   );
 }
+
+interface Props {
+  title: string;
+  previewText: string;
+  publishedDate: string;
+  previewImage: string;
+  postID: number;
+}
+
+const PostCard = ({
+  title,
+  previewImage,
+  previewText,
+  publishedDate,
+  postID,
+}: Props) => (
+  <Link
+    href={`/posts/${postID}`}
+    key={postID}
+    className="group rounded-xl transition-all duration-500 hover:!opacity-100 group-hover/container:opacity-50"
+    onClick={() =>
+      sessionStorage.setItem("position", window.scrollY.toString())
+    }
+  >
+    <div className="relative h-[24rem] w-full max-w-md overflow-hidden rounded-xl transition-all">
+      <div className="absolute inset-0 h-full shrink-0">
+        <Image
+          src={previewImage}
+          alt=""
+          fill
+          className="rounded-xl object-cover object-top transition-all duration-500 group-hover:scale-105"
+          sizes="(max-width: 712px) 100vw, (max-width: 1072px) 50vw, 33vw"
+        />
+      </div>
+      <article className="relative z-10 h-full rounded-md bg-gradient-to-b from-transparent to-base-900 p-6">
+        <div className="relative top-16 flex h-full flex-col justify-end gap-3 transition-all duration-500 group-hover:top-0">
+          <time
+            dateTime={publishedDate}
+            className="text-extrasmall text-base-300"
+          >
+            {formatDate(new Date(publishedDate))}
+          </time>
+          <h2 className="text-large line-clamp-3 max-w-xs font-semibold text-base-200">
+            {title}
+          </h2>
+          <p className="line-clamp-2 text-transparent transition-all duration-500 group-hover:text-base-300">
+            {previewText}
+          </p>
+        </div>
+      </article>
+    </div>
+  </Link>
+);
