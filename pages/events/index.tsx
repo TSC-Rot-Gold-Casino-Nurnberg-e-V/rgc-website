f41835cc899c6273682d38dac1da26a1e7c9ea51
vitaliasvirskaja <vitalia.svirskaja.vs@gmail.com>
2023-06-19 17:07:38 +0200

    #7 implemented usage of slugs for events

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,58 +1,42 @@
 import { GetStaticProps, InferGetStaticPropsType } from "next";
-import Link from "next/link";
-import Image from "next/image";
-import trophyImage from "../../public/trophy.jpg";
-import eventImage from "../../public/eventImage.png";
+import { getEvents } from "../../api/api";
+import { Event } from "../../model/Event";
+import { EventCard } from "../../components/EventCard";
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getStaticProps: GetStaticProps<{ events: Event[] }> = async () => {
+  const events = await getEvents();
+  const dayInSeconds = 60 * 60 * 24;
   return {
-    props: {},
+    props: { events: events, revalidate: dayInSeconds },
   };
 };
 
-export default function Events({}: InferGetStaticPropsType<
-  typeof getStaticProps
->) {
+export default function Events({
+  events,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
-    <main className="mx-auto max-w-3xl grow bg-stone-50">
-      <div className="flex w-full items-center justify-between gap-8">
-        <Link
-          href="/events/competitionResults"
-          className="group relative flex grow flex-col items-center justify-center"
-        >
-          <div className="relative h-80 w-full">
-            <Image
-              src={trophyImage}
-              alt=""
-              fill
-              className="rounded-md bg-center object-cover"
-            />
+    <main className="default-padding mx-auto">
+      <div className="m-auto flex max-w-3xl flex-col gap-8">
+        {events.length !== 0 ? (
+          <div>
+            {events.map((event) => (
+              <EventCard
+                slug={event.attributes.slug}
+                title={event.attributes.title}
+                previewText={event.attributes.previewText}
+                startDate={new Date(event.attributes.startDate)}
+                endDate={
+                  event.attributes.endDate !== null
+                    ? new Date(event.attributes.endDate)
+                    : null
+                }
+                key={event.attributes.slug}
+              />
+            ))}
           </div>
-          <div className="absolute flex h-full w-full flex-col justify-center rounded-md backdrop-brightness-50 duration-700 group-hover:backdrop-brightness-75">
-            <h2 className="w-full px-2 py-5 text-center text-2xl font-bold text-white">
-              Turnierergebnisse
-            </h2>
-          </div>
-        </Link>
-
-        <Link
-          href="/events/eventsOverview"
-          className="group relative flex grow flex-col items-center justify-center"
-        >
-          <div className="relative h-80 w-full">
-            <Image
-              src={eventImage}
-              alt=""
-              fill
-              className="rounded-md bg-center object-cover"
-            />
-          </div>
-          <div className="absolute flex h-full w-full flex-col justify-center rounded-md backdrop-brightness-50 duration-700 group-hover:backdrop-brightness-75">
-            <h2 className="w-full px-2 py-5 text-center text-2xl font-bold text-white">
-              Veranstaltungen
-            </h2>
-          </div>
-        </Link>
+        ) : (
+          <div>Keine Veranstaltungen verfügbar</div>
+        )}
       </div>
     </main>
   );
