f57a5c43b7fedddc15be2973d845b1133218f6d5
vitaliasvirskaja <vitalia.svirskaja.vs@gmail.com>
2023-07-09 11:02:43 +0200

    #44 added property "lizenzen" to trainer model

--- model/Trainer.ts
+++ model/Trainer.ts
@@ -1,11 +1,15 @@
 import { z } from "zod";
+import { lizenzSchema } from "./Lizenz";
 
 export const trainerSchema = z.object({
   id: z.number(),
   attributes: z.object({
     name: z.string(),
-    description: z.string().nullable(),
+    description: z.string(),
     licence: z.string(),
+    lizenzen: z.object({
+      data: z.array(lizenzSchema),
+    }),
     image: z.object({
       data: z.object({
         attributes: z.object({
