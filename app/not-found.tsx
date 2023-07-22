2aa389868feaef09cd30a9459559187e02ff6e90
Vitalia Svirskaja <vitalia.svirskaja.vs@gmail.com>
2023-07-22 20:36:43 +0200

    fixed background color of main content area
    #61

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,6 @@
 export default function NotFound() {
   return (
-    <main className="default-padding mx-auto max-w-3xl grow bg-stone-50">
+    <main className="default-padding mx-auto max-w-3xl grow">
       404 - Sorry bro!
     </main>
   );
