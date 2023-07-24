ffd5b1b2dcea7bb9a8771c90382df59f0129fdbf
Vitalia Svirskaja <vitalia.svirskaja.vs@gmail.com>
2023-07-24 22:41:18 +0200

    fixed issue where bold headings had incorrect color
    #34

--- components/Prose.tsx
+++ components/Prose.tsx
@@ -25,7 +25,6 @@ export const Prose = ({
     prose-p:text-base-600
     prose-a:text-accent
     prose-strong:font-bold
-    prose-strong:text-base-900
     prose-li:accent-secondary-900
     prose-img:max-h-[24rem]
     prose-img:w-full
