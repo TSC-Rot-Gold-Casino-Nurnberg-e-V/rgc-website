da351bc1dbdedab22e8cc6141275878a4a66914c
Vitalia Svirskaja <vitalia.svirskaja.vs@gmail.com>
2023-08-04 22:40:35 +0200

    extracted mail icon into separate component
    #70

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,6 +7,7 @@ import React, {
   SVGProps,
 } from "react";
 import logo from "../public/RGC_Logo_white.svg";
+import { MailIcon } from "./MailIcon";
 
 export const Footer = () => (
   <footer className="bg-base-950">
@@ -155,24 +156,6 @@ const MapIcon = () => (
       strokeLinecap="round"
       strokeLinejoin="round"
       d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
-    />
-  </svg>
-);
-
-const MailIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    strokeWidth={1.5}
-    stroke="currentColor"
-    className="h-6 w-6"
-    role="mailIcon"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
     />
   </svg>
 );
