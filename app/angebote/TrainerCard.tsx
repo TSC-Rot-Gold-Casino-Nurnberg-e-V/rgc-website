7e8432343cc0a5c54c566d39630fa3103678b962
Vitalia Svirskaja <vitalia.svirskaja.vs@gmail.com>
2023-08-06 10:24:19 +0200

    only display licenses of trainers if trainer has at least one license
    #71

--- app/angebote/TrainerCard.tsx
+++ app/angebote/TrainerCard.tsx
@@ -21,16 +21,18 @@ export const TrainerCard = ({ name, lizenzen, bild, beschreibung }: Props) => (
     <section className="space-y-4 sm:space-y-6">
       <div className="space-y-2">
         <h3 className="heading-extrasmall pt-2 text-base-800">{name}</h3>
-        <div className="flex flex-wrap gap-2">
-          {lizenzen.map((lizenz) => (
-            <div
-              key={lizenz.id}
-              className="badge-outline badge px-4 py-3 text-accent"
-            >
-              {lizenz.attributes.name}
-            </div>
-          ))}
-        </div>
+        {lizenzen.length > 0 && (
+          <div className="flex flex-wrap gap-2">
+            {lizenzen.map((lizenz) => (
+              <div
+                key={lizenz.id}
+                className="badge-outline badge px-4 py-3 text-accent"
+              >
+                {lizenz.attributes.name}
+              </div>
+            ))}
+          </div>
+        )}
       </div>
       <Prose content={beschreibung} />
     </section>
