02bfc21154cb1bf1321982544aa5dedc2912c272
Vitalia Svirskaja <vitalia.svirskaja.vs@gmail.com>
2023-08-12 18:21:16 +0200

    removed date section
    #38

--- app/veranstaltungen/[slug]/page.tsx
+++ app/veranstaltungen/[slug]/page.tsx
@@ -1,5 +1,4 @@
 import { getSlugs, getVeranstaltung } from "../../../api/api";
-import { formatDate } from "../../../utils/formatDate";
 import { Prose } from "../../../components/Prose";
 
 export const generateStaticParams = async () => {
@@ -15,21 +14,8 @@ interface Props {
 
 export default async function VeranstaltungPage({ params }: Props) {
   const veranstaltung = await getVeranstaltung(params.slug);
-  const formattedStartDate = formatDate(
-    new Date(veranstaltung.attributes.start)
-  );
   return (
-    <main className="container-md space-y-8">
-      <p className="text-small text-center tracking-widest opacity-70">
-        {veranstaltung.attributes.ende !== null ? (
-          <>
-            {formattedStartDate} {" bis "}
-            {formatDate(new Date(veranstaltung.attributes.ende))}
-          </>
-        ) : (
-          <>{formattedStartDate}</>
-        )}
-      </p>
+    <main className="container-md">
       <Prose
         className="prose-lg"
         content={veranstaltung.attributes.beschreibung}
