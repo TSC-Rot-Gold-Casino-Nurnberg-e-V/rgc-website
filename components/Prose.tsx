e9322b55b5bc1ab93a9058eb37d866b128f24503
Vitalia Svirskaja <vitalia.svirskaja.vs@gmail.com>
2023-08-12 18:56:03 +0200

    configured tailwind-merge for prose to prevent styling and composition issues
    #38

--- components/Prose.tsx
+++ components/Prose.tsx
@@ -1,33 +1,32 @@
 import { ComponentPropsWithoutRef } from "react";
 import { sanitizeHTMLField } from "../utils/sanitizeHTMLField";
+import { twMerge } from "tailwind-merge";
 
 interface Props {
   content: string;
 }
 
-// TODO: use tailwind-merge for className overrides
 export const Prose = ({
   content,
-  className = "",
+  className,
   ...props
 }: Props & Omit<ComponentPropsWithoutRef<"div">, "children">) => (
   <div
-    className={`
-    prose
-    prose-headings:hyphens-auto
-    prose-headings:text-accent
-    prose-p:hyphens-auto
-    prose-p:text-base-600
-    prose-a:text-accent
-    prose-strong:font-bold
-    prose-img:max-h-[24rem]
-    prose-img:w-full
-    prose-img:rounded-xl
-    prose-img:object-cover
-    prose-img:object-top
-    sm:prose-img:max-h-[32rem] 
-    ${className}
-    `}
+    className={twMerge(
+      "prose",
+      "prose-headings:hyphens-auto",
+      "prose-headings:text-accent",
+      "prose-p:hyphens-auto",
+      "prose-p:text-base-600",
+      "prose-a:text-accent",
+      "prose-img:max-h-[24rem]",
+      "prose-img:w-full",
+      "prose-img:rounded-xl",
+      "prose-img:object-cover",
+      "prose-img:object-top",
+      "sm:prose-img:max-h-[32rem]",
+      className
+    )}
     {...props}
     dangerouslySetInnerHTML={{ __html: sanitizeHTMLField(content) }}
   />
