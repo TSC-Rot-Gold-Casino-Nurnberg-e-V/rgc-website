f2c4f44d73208364a4493bd2ffcae08f42ef75c4
vitalia <vitalia.svirskaja.vs@gmail.com>
2023-08-19 15:55:30 +0200

    decreased animation duration

--- components/NeuigkeitCard.tsx
+++ components/NeuigkeitCard.tsx
@@ -30,7 +30,7 @@ export const NeuigkeitCard = ({
   <Link
     href={`/neuigkeiten/${slug}`}
     className={twMerge(
-      "group rounded-xl transition-all duration-500 hover:scale-105",
+      "group rounded-xl transition-all duration-300 hover:scale-105",
       className
     )}
     onClick={() => {
@@ -44,20 +44,20 @@ export const NeuigkeitCard = ({
           src={vorschaubild}
           alt=""
           fill
-          className="rounded-xl object-cover object-top transition-all duration-500 group-hover:scale-105"
+          className="rounded-xl object-cover object-top transition-all duration-300 group-hover:scale-105"
           sizes={sizes}
           priority={loadImageWithPriority}
         />
       </div>
       <article className="relative z-10 h-full rounded-md bg-gradient-to-b from-transparent to-base-900 p-6">
-        <div className="relative top-16 flex h-full flex-col justify-end gap-3 transition-all duration-500 group-hover:top-0">
+        <div className="relative top-16 flex h-full flex-col justify-end gap-3 transition-all duration-300 group-hover:top-0">
           <time dateTime={datum} className="text-extrasmall text-base-300">
             {formatDate(new Date(datum))}
           </time>
           <h2 className="text-large line-clamp-3 max-w-xs font-semibold text-base-200">
             {titel}
           </h2>
-          <p className="line-clamp-2 text-transparent transition-all duration-500 group-hover:text-base-300">
+          <p className="line-clamp-2 text-transparent transition-all duration-300 group-hover:text-base-300">
             {vorschautext}
           </p>
         </div>
