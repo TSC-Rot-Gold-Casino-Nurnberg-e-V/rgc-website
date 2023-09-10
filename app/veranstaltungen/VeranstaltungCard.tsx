2a31ebd6769c4d12ddfb148cfe5a1c86ce1af276
vitalia <vitalia.svirskaja.vs@gmail.com>
2023-09-10 21:17:42 +0200

    decreased size of too large heading

--- app/veranstaltungen/VeranstaltungCard.tsx
+++ app/veranstaltungen/VeranstaltungCard.tsx
@@ -18,18 +18,18 @@ export const VeranstaltungCard = ({ slug, titel, start, ende, ort }: Props) => (
   >
     <div className="flex rounded-xl bg-base-50 shadow transition-shadow hover:shadow-md max-sm:flex-col">
       <div className="flex items-center justify-center bg-secondary-900 px-6 py-4 text-center max-sm:gap-2 max-sm:rounded-t-xl sm:w-24 sm:flex-col sm:rounded-l-xl">
-        <div className="text-gold heading-normal border border-transparent font-extrabold">
+        <div className="text-gold heading-small sm:heading-normal border border-transparent font-extrabold">
           {start.getDate()}
         </div>
-        <div className="text-gold heading-small sm:heading-extrasmall font-semibold">
+        <div className="text-gold heading-extrasmall sm:heading-normal sm:heading-extrasmall font-semibold">
           {start.toLocaleString("de-DE", { month: "short" })}
         </div>
       </div>
       <div className="w-full space-y-6 overflow-hidden p-6 max-sm:pt-4">
-        <h2 className="heading-small line-clamp-3 transition-colors group-hover:text-accent group-focus:text-accent max-sm:hyphens-auto sm:line-clamp-2">
+        <h2 className="heading-extrasmall sm:heading-small line-clamp-3 transition-colors group-hover:text-accent group-focus:text-accent max-sm:hyphens-auto sm:line-clamp-2">
           {titel}
         </h2>
-        <div className="flex justify-between gap-4 max-sm:flex-col">
+        <div className="text-normal sm:text-large flex justify-between gap-4 max-sm:flex-col">
           <div className="flex gap-2">
             <div className="min-w-fit">
               <LocationIcon />
