b3722be0a62466da6d9e032490c159f24c629adc
vitalia <vitalia.svirskaja.vs@gmail.com>
2023-09-27 22:46:50 +0200

    implemented schema "Team"
    #54

---
+++ model/Team.ts
@@ -0,0 +1,40 @@
+import { z } from "zod";
+import { trainerSchema } from "./Trainer";
+import { personSchema } from "./Person";
+import { choreoSchema } from "./Choreo";
+
+export const teamSchema = z
+  .object({
+    id: z.number(),
+    attributes: z.object({
+      titel: z.string(),
+      trainers: z
+        .object({
+          data: z.array(trainerSchema),
+        })
+        .transform(({ data }) => data),
+      kapitaene: z
+        .object({
+          data: z.array(personSchema),
+        })
+        .transform(({ data }) => data),
+      mitglieder: z
+        .object({
+          data: z.array(personSchema),
+        })
+        .transform(({ data }) => data),
+      choreo: z
+        .object({
+          data: choreoSchema,
+        })
+        .transform(({ data }) => data),
+      liga: z
+        .object({
+          data: choreoSchema,
+        })
+        .transform(({ data }) => data),
+    }),
+  })
+  .transform(({ id, attributes }) => ({ id, ...attributes }));
+
+export type Team = z.infer<typeof teamSchema>;
