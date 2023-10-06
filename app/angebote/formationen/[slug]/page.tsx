672a65e42aa8cd84f16aab22d9421736b192cea9
vitalia <vitalia.svirskaja.vs@gmail.com>
2023-10-06 21:56:08 +0200

    implemented tab-view for all teams of a formation
    #54

--- app/angebote/formationen/[slug]/page.tsx
+++ app/angebote/formationen/[slug]/page.tsx
@@ -2,8 +2,7 @@ import { getFormation, getSlugs } from "../../../../api/api";
 import { Main } from "../../../../components/Main";
 import { PageHeading } from "../../../../components/PageHeading";
 import { Prose } from "../../../../components/Prose";
-import { TrainerCard } from "../../TrainerCard";
-import Image from "next/image";
+import { FormationTabs } from "../FormationTabs";
 
 export const generateStaticParams = async () => {
   const slugs = await getSlugs("formationen");
@@ -22,49 +21,8 @@ export default async function FormationPage({ params }: Props) {
   return (
     <Main>
       <PageHeading>{formation.titel}</PageHeading>
-      <Prose content={formation.beschreibung} className="container-lg" />
-      {formation.teams.map((team) => (
-        <>
-          <div key={team.id}>
-            <h2>Titel: {team.titel}</h2>
-            <p>Liga: {team.liga.name}</p>
-            <p>Choreo: {team.choreo.name}</p>
-            {team.trainers.map((trainer) => (
-              <TrainerCard
-                key={trainer.id}
-                name={trainer.person.vorname + " " + trainer.person.nachname}
-                lizenzen={[]}
-                beschreibung={trainer.beschreibung}
-                bild={trainer.person.bild.url}
-              />
-            ))}
-            <h2>Kapitäne:</h2>
-            {team.kapitaene.map((kapitaen) => (
-              <div key={kapitaen.id}>
-                <p>{kapitaen.vorname}</p>
-                <Image
-                  src={kapitaen.bild.url}
-                  alt=""
-                  width={100}
-                  height={100}
-                />
-              </div>
-            ))}
-            <h2>Mitglieder:</h2>
-            {team.mitglieder.map((mitglied) => (
-              <div key={mitglied.id}>
-                <p>{mitglied.vorname}</p>
-                <Image
-                  src={mitglied.bild.url}
-                  alt=""
-                  width={100}
-                  height={100}
-                />
-              </div>
-            ))}
-          </div>
-        </>
-      ))}
+      <Prose content={formation.beschreibung} className="container-md" />
+      <FormationTabs teams={formation.teams} />
     </Main>
   );
 }
