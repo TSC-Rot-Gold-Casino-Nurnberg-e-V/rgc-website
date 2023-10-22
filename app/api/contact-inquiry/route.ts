a33815fccf309ea51f215f43b5c10e94738b697d
vitalia <vitalia.svirskaja.vs@gmail.com>
2023-10-22 11:45:39 +0200

    replaced resend with nodemailer (WIP)
    #28

--- app/api/contact-inquiry/route.ts
+++ app/api/contact-inquiry/route.ts
@@ -1,9 +1,27 @@
-import { Resend } from "resend";
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 import ContactInquiryConfirmationEmail from "../../../emails/ContactInquiryConfirmationEmail";
+import { createTransport } from "nodemailer";
+import { render } from "@react-email/render";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// TODO: replace with test email address (env variable?)
+const RGC_MAIL_ADDRESS = '"RGC TEST 👻" <foo@example.com>';
+
+const transporter = createTransport({
+  host: "smtp.strato.de", // TODO: use strato smtp server host
+  port: 465, // TODO: use strato smtp server port
+  secure: true,
+  // TODO: configure auth (user & pass OR accessToken)
+  auth: {
+    user: "REPLACE",
+    pass: "REPLACE",
+    accessToken: "REPLACE",
+  },
+});
+
+const contactInquiryConfirmationEmailHTML = render(
+  ContactInquiryConfirmationEmail()
+);
 
 const contactInquirySchema = z.object({
   email: z.string().email(),
@@ -14,19 +32,29 @@ export async function POST(request: NextRequest) {
   const body = await request.json();
   const contactInquiry = contactInquirySchema.parse(body);
 
-  console.log("contactInquiry: ", contactInquiry);
+  console.info("Received contact inquiry: ", contactInquiry);
 
   try {
-    const response = await resend.emails.send({
-      from: "RGC TEST <onboarding@resend.dev>",
+    // TODO: macht es überhaupt Sinn, eine separate Confirmation-Email zu verschicken?
+    //  Sollte vielleicht einfach der Absender in CC genommen werden bei der Mail an info@rgc?
+    await transporter.sendMail({
+      from: RGC_MAIL_ADDRESS,
       to: contactInquiry.email,
       subject: "Kontaktanfrage TSC Rot-Gold-Casino Nürnberg e.V.",
-      react: ContactInquiryConfirmationEmail(),
+      html: contactInquiryConfirmationEmailHTML,
     });
 
-    console.log("response: ", response);
+    await transporter.sendMail({
+      from: RGC_MAIL_ADDRESS,
+      to: RGC_MAIL_ADDRESS,
+      subject: "Kontaktanfrage TSC Rot-Gold-Casino Nürnberg e.V.",
+      text: contactInquiry.message,
+    });
 
-    // TODO: contact-inquiry content of contact inquiry to info@rot-gold-casino.de?
+    console.info(
+      "Successfully sent contact inquiry confirmation email to: ",
+      contactInquiry.email
+    );
 
     return NextResponse.json({
       message: "Inquiry successfully processed",
