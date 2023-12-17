da493cb41ee3b918c374fad0c9513eefb945880f
Daniel Merkl <daniel.merkl.dm@gmail.com>
2023-12-17 20:26:53 +0100

    fix performance issues (parallax scroll)

--- app/HeroSection.tsx
+++ app/HeroSection.tsx
@@ -1,47 +1,32 @@
 "use client";
 
-import { PropsWithChildren, useEffect, useRef } from "react";
+import { PropsWithChildren } from "react";
 import { Transition } from "@headlessui/react";
+import { Parallax, ParallaxProvider } from "react-scroll-parallax";
 
-export const HeroSection = ({ children }: PropsWithChildren) => {
-  const heroImageContainer = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    function handleScroll() {
-      heroImageContainer.current?.setAttribute(
-        "style",
-        `transform: translateY(${window.scrollY * 0.5}px)`
-      );
-    }
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
-  return (
-    <section className="relative h-[calc(100dvh-80px)] bg-base-900">
-      <Transition
-        show
-        appear
-        enter="duration-1000 ease-in-out"
-        enterFrom="scale-95 opacity-0"
-        enterTo="scale-100 opacity-100"
-        className="container-lg relative z-10 flex h-full flex-col justify-center space-y-6 text-primary-50"
-      >
-        <h1 className="heading-large sm:heading-extralarge text-gold mx-auto max-w-lg text-center font-extrabold uppercase">
-          Lebe, Liebe, Tanze!
-        </h1>
-        <div className="heading-extrasmall mx-auto max-w-2xl space-y-1 text-center text-base-50">
-          <p>Herzlich Willkommen</p>
-          <p>im TSC Rot-Gold-Casino Nürnberg e.V.</p>
-          <p className="pt-4">Dein Verein für Tanzsport in Nürnberg / Fürth</p>
-        </div>
-      </Transition>
-      <div
-        ref={heroImageContainer}
-        className="absolute inset-0 h-full opacity-50 blur-xs"
-      >
-        {children}
+export const HeroSection = ({ children }: PropsWithChildren) => (
+  <section className="h-[100dvh] bg-base-900">
+    <Transition
+      show
+      appear
+      enter="duration-1000 ease-in-out"
+      enterFrom="scale-95 opacity-0"
+      enterTo="scale-100 opacity-100"
+      className="container-lg relative z-10 flex h-full flex-col justify-center space-y-6 text-primary-50"
+    >
+      <h1 className="heading-large sm:heading-extralarge text-gold mx-auto max-w-lg text-center font-extrabold uppercase">
+        Lebe, Liebe, Tanze!
+      </h1>
+      <div className="heading-extrasmall mx-auto max-w-2xl space-y-1 text-center text-base-50">
+        <p>Herzlich Willkommen</p>
+        <p>im TSC Rot-Gold-Casino Nürnberg e.V.</p>
+        <p className="pt-4">Dein Verein für Tanzsport in Nürnberg / Fürth</p>
       </div>
-    </section>
-  );
-};
+    </Transition>
+    <ParallaxProvider>
+      <Parallax speed={-50} className="absolute inset-0 opacity-50 blur-xs">
+        {children}
+      </Parallax>
+    </ParallaxProvider>
+  </section>
+);
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,97 +2,86 @@
 
 import Link from "next/link";
 import { Menu, Transition } from "@headlessui/react";
-import { useHideNavbar } from "../utils/useHideNavbar";
 import React, { AnchorHTMLAttributes, forwardRef } from "react";
 import { usePathname } from "next/navigation";
 import { twJoin, twMerge } from "tailwind-merge";
 import { RgcIcon } from "./icons/RgcIcon";
 
-export const Navbar = () => {
-  const hideNavbar = useHideNavbar();
-  return (
-    <nav
-      className={twJoin(
-        "sticky z-30 h-20 w-full bg-base-900 px-8 text-base-50 transition-all duration-500",
-        hideNavbar ? "-top-20" : "top-0"
-      )}
-    >
-      <div className="mx-auto flex h-20 max-w-screen-lg items-center justify-between">
-        <div className="h-full w-fit py-4">
-          <Link
-            href="/"
-            aria-label="Startseite"
-            className="block h-full w-fit rounded-full"
-          >
-            <RgcIcon />
-          </Link>
-        </div>
-        <ul className="flex gap-1 max-md:hidden">
-          <NavLink text="Der Verein" href="/verein" />
-          <NavLink text="News" href="/neuigkeiten" />
-          <NavLink text="Angebot" href="/angebote" />
-          <NavLink text="Veranstaltungen" href="/veranstaltungen" />
-          <NavLink
-            text="Turnierergebnisse"
-            href="/turnierergebnisse"
-            shouldHideOnSmallViewport
-          />
-          <NavLink text="Kontakt" href="/kontakt" />
-        </ul>
-        <div className="relative md:hidden">
-          <Menu>
-            {({ open }) => (
-              <>
-                <Menu.Button
-                  className="rounded-md p-2"
-                  aria-label={`Navigationsmenü ${
-                    open ? "schließen" : "öffnen"
-                  }`}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="h-6 w-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                    />
-                  </svg>
-                </Menu.Button>
-                <Transition
-                  className="absolute -right-4 z-10"
-                  enter="transition duration-100 ease-out"
-                  enterFrom="transform scale-95 opacity-0"
-                  enterTo="transform scale-100 opacity-100"
-                  leave="transition duration-75 ease-out"
-                  leaveFrom="transform scale-100 opacity-100"
-                  leaveTo="transform scale-95 opacity-0"
-                >
-                  <Menu.Items className="text-normal rounded-2xl bg-base-800 p-2 text-base-50 shadow-sm shadow-base-900">
-                    <MenuLink text="Der Verein" href="/verein" />
-                    <MenuLink text="News" href="/neuigkeiten" />
-                    <MenuLink text="Angebot" href="/angebote" />
-                    <MenuLink text="Veranstaltungen" href="/veranstaltungen" />
-                    <MenuLink
-                      text="Turnierergebnisse"
-                      href="/turnierergebnisse"
-                    />
-                    <MenuLink text="Kontakt" href="/kontakt" />
-                  </Menu.Items>
-                </Transition>
-              </>
-            )}
-          </Menu>
-        </div>
+export const Navbar = () => (
+  <nav className="sticky top-0 z-30 h-16 w-full bg-base-900 px-8 text-base-50 transition-all duration-500 sm:h-20">
+    <div className="mx-auto flex h-16 max-w-screen-lg items-center justify-between sm:h-20">
+      <div className="h-full w-fit py-4">
+        <Link
+          href="/"
+          aria-label="Startseite"
+          className="block h-full w-fit rounded-full"
+        >
+          <RgcIcon />
+        </Link>
       </div>
-    </nav>
-  );
-};
+      <ul className="flex gap-1 max-md:hidden">
+        <NavLink text="Der Verein" href="/verein" />
+        <NavLink text="News" href="/neuigkeiten" />
+        <NavLink text="Angebot" href="/angebote" />
+        <NavLink text="Veranstaltungen" href="/veranstaltungen" />
+        <NavLink
+          text="Turnierergebnisse"
+          href="/turnierergebnisse"
+          shouldHideOnSmallViewport
+        />
+        <NavLink text="Kontakt" href="/kontakt" />
+      </ul>
+      <div className="relative md:hidden">
+        <Menu>
+          {({ open }) => (
+            <>
+              <Menu.Button
+                className="rounded-md p-2"
+                aria-label={`Navigationsmenü ${open ? "schließen" : "öffnen"}`}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="h-6 w-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                  />
+                </svg>
+              </Menu.Button>
+              <Transition
+                className="absolute -right-4 z-10"
+                enter="transition duration-100 ease-out"
+                enterFrom="transform scale-95 opacity-0"
+                enterTo="transform scale-100 opacity-100"
+                leave="transition duration-75 ease-out"
+                leaveFrom="transform scale-100 opacity-100"
+                leaveTo="transform scale-95 opacity-0"
+              >
+                <Menu.Items className="text-normal rounded-2xl bg-base-800 p-2 text-base-50 shadow-sm shadow-base-900">
+                  <MenuLink text="Der Verein" href="/verein" />
+                  <MenuLink text="News" href="/neuigkeiten" />
+                  <MenuLink text="Angebot" href="/angebote" />
+                  <MenuLink text="Veranstaltungen" href="/veranstaltungen" />
+                  <MenuLink
+                    text="Turnierergebnisse"
+                    href="/turnierergebnisse"
+                  />
+                  <MenuLink text="Kontakt" href="/kontakt" />
+                </Menu.Items>
+              </Transition>
+            </>
+          )}
+        </Menu>
+      </div>
+    </div>
+  </nav>
+);
 
 interface NavLinkProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
   text: string;
