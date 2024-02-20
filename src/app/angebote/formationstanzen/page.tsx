e6dc6229b99ce603afd9e2c8e72b19b30105ee63
Daniel Merkl <daniel.merkl.dm@gmail.com>
2024-02-20 18:20:31 +0100

    heading left aligned on mobile screens

--- src/app/angebote/formationstanzen/page.tsx
+++ src/app/angebote/formationstanzen/page.tsx
@@ -19,7 +19,7 @@ export default function FormationstanzenPage() {
         </p>
       </div>
       <section className="container-lg space-y-8">
-        <h2 className="heading-normal text-accent text-center">Tanzarten</h2>
+        <h2 className="heading-normal text-accent sm:text-center">Tanzarten</h2>
         <div className="grid items-center gap-8 sm:grid-cols-2 sm:gap-y-12">
           <section className="space-y-4 self-center sm:order-3">
             <h2 className="heading-small text-accent sm:text-center">
