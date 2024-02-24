8c8ed48815d05805bc065429adfb6252ee5af115
Daniel Merkl <daniel.merkl.dm@gmail.com>
2024-02-24 14:09:26 +0100

    image optimization for CMS content

--- src/components/Prose.tsx
+++ src/components/Prose.tsx
@@ -1,6 +1,8 @@
 import { ComponentPropsWithoutRef } from "react";
-import { sanitizeHTMLField } from "@/utils/sanitizeHTMLField";
 import { twMerge } from "tailwind-merge";
+import Markdown from "react-markdown";
+import Image from "next/image";
+import rehypeRaw from "rehype-raw";
 
 interface Props {
   content: string;
@@ -29,6 +31,38 @@ export const Prose = ({
       className,
     )}
     {...props}
-    dangerouslySetInnerHTML={{ __html: sanitizeHTMLField(content) }}
-  />
+  >
+    <Markdown
+      rehypePlugins={[rehypeRaw]}
+      components={{
+        a: ({ node, ...props }) => <a {...props} target="_blank" />,
+        img: ({ src = "", alt = "", height, width }) => {
+          const DEFAULT_ASPECT_RATIO = 4 / 3;
+          const parsedHeight = Number(height);
+          const parsedWidth = Number(width);
+          const aspectRatio =
+            isNaN(parsedHeight) ||
+            isNaN(parsedWidth) ||
+            parsedHeight === 0 ||
+            parsedWidth === 0
+              ? DEFAULT_ASPECT_RATIO
+              : parsedWidth / parsedHeight;
+
+          return (
+            <div className="relative" style={{ aspectRatio }}>
+              <Image
+                src={src}
+                alt={alt}
+                priority
+                fill
+                sizes="(max-width: 640px) 100vw, 800px"
+              />
+            </div>
+          );
+        },
+      }}
+    >
+      {content}
+    </Markdown>
+  </div>
 );
