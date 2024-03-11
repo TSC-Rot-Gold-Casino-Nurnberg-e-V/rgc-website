bc1c4943ddfd00560aa32e61e715442eedc71084
Daniel Merkl <daniel.merkl.dm@gmail.com>
2024-03-11 22:27:50 +0100

    start news card backdrop fade in middle

--- src/components/NeuigkeitCard.tsx
+++ src/components/NeuigkeitCard.tsx
@@ -44,7 +44,7 @@ export const NeuigkeitCard = ({
           priority={loadImageWithPriority}
         />
       </div>
-      <article className="relative z-10 h-full rounded-xl bg-gradient-to-b from-transparent to-base-900 p-6">
+      <article className="relative z-10 h-full rounded-xl bg-gradient-to-b from-transparent from-50% to-base-900 p-6">
         <div className="relative top-20 flex h-full flex-col justify-end gap-3 transition-all duration-300 sm:group-hover:top-0">
           <time dateTime={datum} className="text-extrasmall text-base-300">
             {formatDate(new Date(datum))}
