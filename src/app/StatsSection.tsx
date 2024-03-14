9e9f3bd1c9eb0eaedef96fdb5c3d3c90ea4a9f90
Daniel Merkl <daniel.merkl.dm@gmail.com>
2024-03-14 22:54:33 +0100

    improve design of stats section

--- src/app/StatsSection.tsx
+++ src/app/StatsSection.tsx
@@ -14,25 +14,34 @@ export const StatsSection = () => {
       aria-label="Vereinsstatistik"
       ref={ref}
     >
-      <div className="text-gold container-lg grid w-full gap-6 py-12 text-center font-bold max-sm:max-w-sm max-sm:px-6 sm:grid-cols-3 md:justify-between">
-        <div className="w-40 max-sm:justify-self-start" aria-label="Statistik">
+      <div className="container-lg grid w-full gap-6 py-12 text-center font-bold max-sm:max-w-sm max-sm:px-6 sm:grid-cols-3 md:justify-between">
+        <div
+          className="text-gold w-40 max-sm:justify-self-start"
+          aria-label="Statistik"
+        >
           <div className="text-5xl">
             {">"}
             <CountUp end={inView ? 600 : 0} />
           </div>
-          <div className="text-base sm:text-lg">Mitglieder</div>
+          <div className="text-xl sm:text-2xl">Mitglieder</div>
         </div>
-        <div className="max-sm:justify-self-end" aria-label="Statistik">
+        <div
+          className="text-gold max-sm:justify-self-end"
+          aria-label="Statistik"
+        >
           <div className="text-5xl">
             <CountUp end={inView ? 8 : 0} />
           </div>
-          <div className="text-base sm:text-lg">Formationsteams</div>
+          <div className="text-xl sm:text-2xl">Formations&shy;teams</div>
         </div>
-        <div className="max-sm:justify-self-start" aria-label="Statistik">
+        <div
+          className="text-gold max-sm:justify-self-start"
+          aria-label="Statistik"
+        >
           <div className="text-5xl">
             <CountUp end={inView ? 26 : 0} />x
           </div>
-          <div className="text-base sm:text-lg">Bayernpokalsieger</div>
+          <div className="text-xl sm:text-2xl">Bayernpokal&shy;sieger</div>
         </div>
       </div>
     </section>
