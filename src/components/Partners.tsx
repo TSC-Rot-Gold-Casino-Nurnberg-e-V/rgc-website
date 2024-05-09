693c9a315f6ad75a3c4ed65ced1a2508fbb33279
Daniel Merkl <daniel.merkl.dm@gmail.com>
2024-05-09 21:41:13 +0200

    infinite auto scroll for partners

--- src/components/Partners.tsx
+++ src/components/Partners.tsx
@@ -4,58 +4,80 @@ import sbbLogo from "../../public/sponsors/sbbLogo.png";
 import teamNbgLogo from "../../public/sponsors/teamnuernbergLogo.jpg";
 import hdKroftLogo from "../../public/sponsors/hdKroftLogo.png";
 import heyduckMalerbetrieb from "../../public/sponsors/heyduckMalerbetrieb.png";
+import bravoDance from "../../public/sponsors/bravoDance.jpeg";
+import { ComponentProps } from "react";
 
 export const Partners = () => (
-  <section className="bg-base-900">
-    <div className="container-lg grid items-center justify-center gap-8 align-middle sm:grid-cols-2 md:grid-cols-4">
-      <Link
-        href="https://www.pictrs.com/hd-kroft-photography?l=de"
-        target="_blank"
-        className="mx-auto rounded-md"
-      >
-        <Image
-          src={hdKroftLogo}
-          alt="HD-Kroft Photography"
-          width={250}
-          className="h-auto rounded-md"
-        />
-      </Link>
-      <Link
-        href="https://www.sportbuendnis-bundesliga.de/"
-        target="_blank"
-        className="mx-auto rounded-full"
-      >
-        <Image
-          src={sbbLogo}
-          alt="Sportbündnis Bundesliga"
-          width={250}
-          className="h-auto rounded-md"
-        />
-      </Link>
-      <Link
-        href="https://www.nuernberg.de/internet/team_nuernberg/"
-        target="_blank"
-        className="mx-auto rounded-md"
-      >
-        <Image
-          src={teamNbgLogo}
-          alt="Team Nürnberg"
-          width={250}
-          className="h-auto rounded-md"
-        />
-      </Link>
-      <Link
-        href="https://www.heyduck-maler.de/"
-        target="_blank"
-        className="mx-auto rounded-md"
-      >
-        <Image
-          src={heyduckMalerbetrieb}
-          alt="Heyduck Maler und Lackierer"
-          width={250}
-          className="h-auto rounded-md"
-        />
-      </Link>
-    </div>
+  <section className="flex overflow-hidden bg-base-900 py-6 sm:py-8 md:py-10">
+    <PartnerList />
+    <PartnerList aria-hidden />
   </section>
+);
+
+const PartnerList = (props: ComponentProps<"div">) => (
+  <div
+    {...props}
+    className="animate-infinite-scroll flex items-center space-x-8 pr-8"
+  >
+    <Link
+      href="https://www.pictrs.com/hd-kroft-photography?l=de"
+      target="_blank"
+      className="mx-auto rounded-md"
+    >
+      <Image
+        src={hdKroftLogo}
+        alt="HD-Kroft Photography"
+        width={250}
+        className="h-auto min-w-[250px] rounded-md"
+      />
+    </Link>
+    <Link
+      href="https://www.sportbuendnis-bundesliga.de/"
+      target="_blank"
+      className="mx-auto rounded-full"
+    >
+      <Image
+        src={sbbLogo}
+        alt="Sportbündnis Bundesliga"
+        width={250}
+        className="h-auto min-w-[250px] rounded-md"
+      />
+    </Link>
+    <Link
+      href="https://www.nuernberg.de/internet/team_nuernberg/"
+      target="_blank"
+      className="mx-auto rounded-md"
+    >
+      <Image
+        src={teamNbgLogo}
+        alt="Team Nürnberg"
+        width={250}
+        className="h-auto min-w-[250px] rounded-md"
+      />
+    </Link>
+    <Link
+      href="https://www.heyduck-maler.de/"
+      target="_blank"
+      className="mx-auto rounded-md"
+    >
+      <Image
+        src={heyduckMalerbetrieb}
+        alt="Heyduck Maler und Lackierer"
+        width={250}
+        className="h-auto min-w-[250px] rounded-md"
+      />
+    </Link>
+    <Link
+      href="https://bravo-dance.com/de/"
+      target="_blank"
+      className="mx-auto rounded-md"
+    >
+      <Image
+        src={bravoDance}
+        alt="Bravo Dance"
+        width={250}
+        className="h-auto min-w-[250px] rounded-md"
+      />
+    </Link>
+  </div>
 );
