a2735bc2f9a52765c774e44528bb8aa983fbf8e1
Daniel Merkl <daniel.merkl.dm@gmail.com>
2024-05-20 15:00:17 +0200

    add "/mitgliedschaft" to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -130,5 +130,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "yearly",
       priority: 0.5,
     },
+    {
+      url: `${BASE_URL}/mitgliedschaft`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
   ];
 }
