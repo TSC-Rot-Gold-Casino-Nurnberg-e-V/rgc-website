4d97ad05dd0e5d0381b4db92ffceaedb24c1028e
Daniel Merkl <daniel.merkl.dm@gmail.com>
2024-10-18 18:39:28 +0200

    add showanfragen in sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -47,6 +47,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.7,
     },
     {
+      url: `${BASE_URL}/showanfrage`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.8,
+    },
+    {
       url: `${BASE_URL}/impressum`,
       lastModified: new Date(),
       changeFrequency: "yearly",
