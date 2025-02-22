86d63e47337498db050f38a491e5bc63cca1e66a
Daniel Merkl <daniel.merkl.dm@gmail.com>
2025-02-22 13:36:21 +0100

    fix url

--- src/app/livestream/TwitchEmbed.tsx
+++ src/app/livestream/TwitchEmbed.tsx
@@ -1,8 +1,9 @@
+const origin = process.env.VERCEL_URL ?? "localhost";
+
 export const TwitchEmbed = () => {
   const url = new URL("https://player.twitch.tv");
   url.searchParams.append("channel", "monstercat");
-  url.searchParams.append("parent", "localhost");
-  url.searchParams.append("parent", "rot-gold-casino.de");
+  url.searchParams.append("parent", origin);
 
   return (
     <div className="bg-base-900">
