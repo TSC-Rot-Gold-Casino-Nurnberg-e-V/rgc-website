b2228c9df31ae60dedf9824534b638556565f094
Daniel Merkl <daniel.merkl.dm@gmail.com>
2025-02-22 22:50:45 +0100

    add shadow

--- src/components/LivestreamChip.tsx
+++ src/components/LivestreamChip.tsx
@@ -13,7 +13,7 @@ export const LiveStreamChip = () => {
   return SHOW_LIVESTREAM && !isLivestreamPage ? (
     <Link
       href="/livestream"
-      className="mx-auto grid w-fit -translate-y-1/2 grid-cols-[auto_1fr_auto] items-center rounded-full bg-base-800 transition-all hover:scale-105 hover:bg-base-700"
+      className="mx-auto grid w-fit -translate-y-1/2 grid-cols-[auto_1fr_auto] items-center rounded-full bg-base-800 shadow transition-all hover:scale-105 hover:bg-base-700"
     >
       <div className="w-fit animate-pulse rounded-l-full bg-secondary-600 py-1 pl-3.5 pr-3 font-bold">
         LIVE
