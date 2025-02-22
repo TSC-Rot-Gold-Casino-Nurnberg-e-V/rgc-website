8d53fbe77d6632712b1c618009890a699ff3ab16
Daniel Merkl <daniel.merkl.dm@gmail.com>
2025-02-22 13:32:41 +0100

    fix url

--- src/app/livestream/TwitchEmbed.tsx
+++ src/app/livestream/TwitchEmbed.tsx
@@ -1,12 +1,19 @@
-export const TwitchEmbed = () => (
-  <div className="bg-base-900">
-    <div className="mx-auto aspect-video max-w-screen-lg">
-      <iframe
-        src="https://player.twitch.tv/?channel=monstercat&parent=localhost"
-        height="100%"
-        width="100%"
-        allowFullScreen
-      />
+export const TwitchEmbed = () => {
+  const url = new URL("https://player.twitch.tv");
+  url.searchParams.append("channel", "monstercat");
+  url.searchParams.append("parent", "localhost");
+  url.searchParams.append("parent", "rot-gold-casino.de");
+
+  return (
+    <div className="bg-base-900">
+      <div className="mx-auto aspect-video max-w-screen-lg">
+        <iframe
+          src={url.toString()}
+          height="100%"
+          width="100%"
+          allowFullScreen
+        />
+      </div>
     </div>
-  </div>
-);
+  );
+};
