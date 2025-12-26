4ccb44aaee6a83fb3464413aa43f23db86786e6a
Daniel Merkl <daniel.merkl.dm@gmail.com>
2025-12-26 20:08:21 +0100

    refactor LinkButton to accept additional props and merge class names

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -1,16 +1,30 @@
 import Link from "next/link";
 import { Button } from "./Button";
 import { ChevronRightIcon } from "./icons/ChevronRightIcon";
+import { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
 
-interface Props {
+interface Props extends ComponentProps<"a"> {
   href: string;
   text: string;
   target?: string;
 }
 
-export const LinkButton = ({ href, text, target }: Props) => (
-  <Link href={href} target={target} className="block w-fit rounded-full">
+export const LinkButton = ({
+  href,
+  text,
+  target,
+  className,
+  ...props
+}: Props) => (
+  <Link
+    href={href}
+    target={target}
+    className={twMerge("block w-fit rounded-full", className)}
+    {...props}
+  >
     <Button
+      className="w-full"
       tabIndex={-1}
       variant="secondary"
       endIcon={<ChevronRightIcon className="size-4" />}
